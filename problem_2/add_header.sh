f225c731e83052d3309f37b22069effcca08ae74
Ben Sathis <ben@sathis.xyz>
2023-11-19 13:13:10 -0800

    Incorrect use of pipe in script. Fixed and cleaned up for legibility (that part was optional)

--- problem_2/add_header.sh
+++ problem_2/add_header.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-
-files_i_care_about=( $(ls *.c))
+files_i_care_about=( $(ls *.c) )
 
 for t in ${!files_i_care_about[@]}; do
-    echo '**Propety of Blaa.co 1882**' | cat - $t > temp && mv temp $t
+    echo '**Propety of Blaa.co 1882**' > temp
+    cat ${files_i_care_about[$t]} >> temp
+    mv temp ${files_i_care_about[$t]}
 done
---
+++ problem_2/test2.c
@@ -0,0 +1,2 @@
+**Propety of Blaa.co 1882**
+This is another test file.
